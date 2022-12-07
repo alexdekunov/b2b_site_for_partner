2ddfb9f01390ced7812dbf0890a8dbcfa706880d
Igoruxa007 <74563405+Igoruxa007@users.noreply.github.com>
2022-12-07 16:28:32 +0300

    с помощью bootstrap создана первая страница

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_page():
-    return "Main page"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
