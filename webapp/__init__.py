41c2c7f6e0d74c5d590b718b9c6adac8803faf76
Dekunov Aleksandr <wresda@gmail.com>
2023-01-08 20:31:34 +0300

    создание базы данных

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,8 +5,8 @@ from webapp.model import db
 
 def create_app():
     app = Flask(__name__)
-    #app.config.from_pyfile('config.py')
-    #db.init_app(app)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     @app.route('/')
     def index():
