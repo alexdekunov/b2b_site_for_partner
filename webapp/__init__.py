4330403f11788d4061c982aad4a6f9ee4f1cde9b
Dekunov Aleksandr <wresda@gmail.com>
2023-01-11 23:38:55 +0300

    fix init and add db

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
 from webapp.forms import LoginForm
+from webapp.model import db, User
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     @app.route('/')
     def index():
