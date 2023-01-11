fd6a58ff37f39027e0e5fae8aac762895aba23c7
alexdekunov <113306381+alexdekunov@users.noreply.github.com>
2023-01-11 17:53:42 +0300

    БД для логина пользователя (#12)

---
+++ webapp/model.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
