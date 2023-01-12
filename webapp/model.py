0d1ac98187c2351b3616c6dffd4ca4fecfd109d4
Aleksandr Dekunov <wresda@gmail.com>
2023-01-12 16:55:28 +0300

    работа над паролем и добавлением пользователя

--- webapp/model.py
+++ webapp/model.py
@@ -1,12 +1,20 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128))
     role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+        
+    def check_password(self, password):
+        return check_password_hach(self.password, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
