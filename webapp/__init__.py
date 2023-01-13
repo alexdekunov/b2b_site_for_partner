a08a08f1948f8a0ae6cd00885e37323434691fa7
alexdekunov <113306381+alexdekunov@users.noreply.github.com>
2023-01-13 17:56:36 +0300

    логаут и алерты при авторизации (#15)

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user
 
 from webapp.forms import LoginForm
 from webapp.model import db, User
@@ -9,23 +10,47 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
     @app.route('/')
     def index():
         title = "Главная страница"
         return render_template('index.html', page_title=title)
     
-
     @app.route('/login')
     def login():
         title = 'Авторизация'
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
 
-    
-
     @app.route('/about')
     def about():
         title = "О нас"
         return render_template('about.html', page_title=title)
+    
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы успешно вошли на сайт')
+                return redirect(url_for('login'))
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+    
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы вышли из личного кабинета')
+        return redirect(url_for('login'))
 
     return app
