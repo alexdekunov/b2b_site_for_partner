cc33c8d24fb7a7b77834fb6ebc0c0af05d566043
Dekunov Aleksandr <wresda@gmail.com>
2023-01-23 18:16:38 +0300

    декоратор проверки админа

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,12 +1,11 @@
-from flask import Blueprint
-from flask_login import current_user, login_required
+from flask import Blueprint, render_template
+
+from webapp.user.decorators import admin_required
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
 @blueprint.route('/')
-@login_required
+@admin_required
 def admin_index():
-    if current_user.is_admin:
-        return 'Привет админ!'
-    else:
-        return 'Ты не админ!'
+    title = "Панель управления"
+    return render_template('admin/index.html', page_title=title)
